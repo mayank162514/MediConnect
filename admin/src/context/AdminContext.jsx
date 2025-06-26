1095e6cb2b91ca247a367f026b575cb460670c8f
mayank162514 <130738930+mayank162514@users.noreply.github.com>
2025-06-26 22:59:08 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -11,7 +11,7 @@ const AdminContextProvider = (props) => {
     const [appointments, setAppointments] = useState([])
     const [dashData, setDashData] = useState(false)
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://mediconnect-backend-2o4q.onrender.com"
 
     const getAllDoctors = async () => {
         try {
