d28a1af22462146677e2baa026bf2ba736b3a5a5
mayank162514 <130738930+mayank162514@users.noreply.github.com>
2025-06-26 22:55:11 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://mediconnect-backend-2o4q.onrender.com"
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(()=>{
